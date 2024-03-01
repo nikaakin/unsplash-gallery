d1d38852d89a14f7fc29edeb37c84311da13bbdb
nikaakin <nikacuckiridze47@gmail.com>
2024-03-01 15:04:17 +0400

    feat: add unsplash services

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Home, History, NotFound } from "@/pages";
+import "./index.css";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
---
+++ src/services/unsplashServices.ts
@@ -0,0 +1,32 @@
+export const getPopular = (page = 1) =>
+  fetch(
+    `https://api.unsplash.com/photos/?order_by=popular&per_page=20&client_id=${
+      import.meta.env.VITE_UNSPLASH_ACCESS_KEY
+    }&page=${page}`
+  )
+    .then((res) => res.json())
+    .catch((error) => {
+      throw new Error(error);
+    });
+
+export const getPhoto = (id: string) =>
+  fetch(
+    `https://api.unsplash.com/photos/${id}?client_id=${
+      import.meta.env.VITE_UNSPLASH_ACCESS_KEY
+    }`
+  )
+    .then((res) => res.json())
+    .catch((error) => {
+      throw new Error(error);
+    });
+
+export const searchPhotos = (query: string, page = 1) =>
+  fetch(
+    `https://api.unsplash.com/search/photos?query=${query}&client_id=${
+      import.meta.env.VITE_UNSPLASH_ACCESS_KEY
+    }&per_page=20&page=${page}`
+  )
+    .then((res) => res.json())
+    .catch((error) => {
+      throw new Error(error);
+    });
